1fce7d0082eca28c8893c71db9eefbda3bad58e0
LuigiOvertoom <firedream2236@gmail.com>
2019-02-04 12:24:00 +0100

    added goal en ball physics

---
+++ Assets/Game/Scripts/Luigi/NetScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetScript : MonoBehaviour
+{
+    [SerializeField]
+    private GoalScript m_Goal;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Good"))
+        {
+            m_Goal.GetComponent<GoalScript>().m_score += 1;
+            m_Goal.GetComponent<GoalScript>().m_yPos = Random.Range(-4, 1);
+        }
+        else if (collision.CompareTag("Bad"))
+        {
+            m_Goal.GetComponent<GoalScript>().m_score -= 1;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Luigi/TempFolder/BallSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    private ObjectPooler m_objectpool;
+
+    private float m_spawnTimer;
+    public float m_startSpawnTimer;
+
+    private float m_decision;
+
+    void Start()
+    {
+        m_objectpool = ObjectPooler.Instance;
+        m_spawnTimer = m_startSpawnTimer;
+        m_decision = 0;
+    }
+    void Update()
+    {
+        if (m_spawnTimer <= 0 && m_decision == 0)
+        {
+            m_decision = 1;
+            m_objectpool.SpawnFromPool("GoodBall", transform.position, Quaternion.identity);
+            m_spawnTimer = m_startSpawnTimer;
+        }
+        else if (m_spawnTimer <= 0 && m_decision == 1)
+        {
+            m_decision = Random.Range(0, 1);
+            m_objectpool.SpawnFromPool("BadBall", transform.position, Quaternion.identity);
+            m_spawnTimer = m_startSpawnTimer;
+        }
+        else
+        {
+            m_spawnTimer -= Time.deltaTime;
+        }
+    }
+}
